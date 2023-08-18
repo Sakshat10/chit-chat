93bbea10a2c8cb86cfbd064334afb7c2c1a9bb7d
Sakshat Kumar <112819804+Sakshat10@users.noreply.github.com>
2023-08-19 01:37:43 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {signOut} from "firebase/auth"
+import { auth } from '../firebase'
+import { AuthContext } from '../context/AuthContext'
 
 const Navbar = () => {
+  const {currentUser} = useContext(AuthContext)
+
   return (
     <div className='navbar'>
       <span className="logo">Chit Chat</span>
       <div className="user">
-        <img src="https://images.pexels.com/photos/16084795/pexels-photo-16084795/free-photo-of-city-fashion-people-woman.jpeg?auto=compress&cs=tinysrgb&w=800" alt="profile-pic" />
-        <span>Chinki</span>
-        <button>Logout</button>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
+        <button onClick={()=>signOut(auth)}>logout</button>
       </div>
     </div>
   )
